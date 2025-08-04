7075df46f3627ca57542ce20810241a79a860f55
Felipe Kravec Zanatta <fkz.zanatt@gmail.com>
2025-08-04 13:36:17 -0300

    diffuse sphere

--- rtweekend.h
+++ rtweekend.h
@@ -6,6 +6,8 @@
 #include <limits>
 #include <cmath>
 #include <iostream>
+#include <cstdlib>
+#include <random>
 
 // Constants
 const double infinity = std::numeric_limits<double>::infinity();
@@ -16,8 +18,22 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+// Utility Functions
+
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
+
 // Common Headers
 #include "color.h"
+#include "interval.h"
 #include "ray.h"
 #include "vec3.h"
 
